df4e7b6da4dde8ccf50de9ac04593c40f931f5d3
yuria <takaii666@gmail.com>
2024-04-12 03:29:08 +0800

    Todo List

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
+//[{text: '洗衣服', checked: false}, {text: '寫文案', checked: true}]
+let listState = [];
+
+const STATE_KEY = "todo-list";
+
+function loadState() {
+    const listState = localStorage.getItem(STATE_KEY);
+    if (listState !== null) {
+        return JSON.parse(listState);
+    }
+    return [];
+}
+
+function saveState(list) {
+    localStorage.setItem(STATE_KEY, JSON.stringify(list));
+}
+
+function initList() {
+    // load state
+    listState = loadState();
+    // render list
+    const ul = document.getElementById("list");
+    for (const item of listState) {
+        const li = document.createElement("li");
+        li.innerText = item.text;
+
+        const deleteButton = document.createElement("span");
+        deleteButton.classList.add("delete");
+        deleteButton.onclick = deleteItem;
+        li.appendChild(deleteButton);
+
+        li.classList.add("item");
+        if (item.checked) {
+            li.classList.add("checked");
+        }
+        li.onclick = checkItem;
+
+        ul.appendChild(li);
+    }
+}
+
 function addItem() {
-    console.log("addItem");
     const ul = document.getElementById("list");
     const input = document.getElementById("input");
     const text = input.value;
@@ -9,7 +49,7 @@ function addItem() {
     }
 
     const newItem = document.createElement("li");
-    newItem.classList.add("item")
+    newItem.classList.add("item");
     newItem.innerText = text;
 
     newItem.onclick = checkItem;
@@ -20,34 +60,43 @@ function addItem() {
 
     newItem.appendChild(deleteButton);
 
+    listState.push({
+        text,
+        checked: false
+    });
+    saveState(listState);
+
     input.value = "";
     ul.appendChild(newItem);
-
-
-
 }
 
-function checkItem() {
-    console.log("check");
-    const item = this;
+function checkItem(e) {
+    const item = e.target;
+    const parent = item.parentNode;
+    const idx = Array.from(parent.childNodes).indexOf(item);
+
+    listState[idx].checked = !listState[idx].checked;
+
     item.classList.toggle("checked");
-
+    saveState(listState);
 }
 
-function deleteItem() {
-    console.log("delete");
-    const item = this.parentNode;  // li
-    // const parent = item.parentNode;  // ul
-    // parent.removeChild(item);
-    item.remove();
+function deleteItem(e) {
+    const item = this.parentNode;
+    const parent = item.parentNode;
+    const idx = Array.from(parent.childNodes).indexOf(item);
+    listState = listState.filter((_, i) => i !== idx);
+    parent.removeChild(item);
+    saveState(listState);
+    e.stopPropagation();
 }
+
+initList();
 
 const addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addItem);
 
 const form = document.getElementById("input-wrapper");
 form.addEventListener("submit", (e) => {
-    // 取消DOM預設功能
     e.preventDefault();
-})
-
+});
